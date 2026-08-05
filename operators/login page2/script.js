0d54518e5cb149c2a3f2545d29f47ac2ea88ca37
IBK-LTD <ajanakuemmanuel032@gmail.com>
2026-08-05 15:25:36 +0100

    arranged the folders

--- operators/login page2/script.js
+++ operators/login page2/script.js
@@ -7,7 +7,7 @@ loginForm.addEventListener("submit", (e) => {
   // alert("Login functionality is currently being integrated with the backend. Coming soon!");
 
   // Optional: uncomment below to redirect straight to dashboard for demo purposes
-  window.location.href = "../dashboard2/index.html";
+  window.location.href = "../dashboard2/dashboard.html";
 });
 
 const requestAccessBtn = document.querySelector(".btn-sso");
